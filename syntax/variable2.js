e74be6b00559fd596a539afbdda770252410fd51
haerang <gofkdvjvl11@gmail.com>
2020-02-01 01:17:59 +0900

    finished settings

---
+++ syntax/variable2.js
@@ -0,0 +1,13 @@
+var name = "haerang";
+var letter = "dear " + name + "\n\
+adf";
+console.log(letter);
+
+// 템플릿 리터럴 사용
+var letter = `dear ${name}
+
+adsfadf ${1 + 1}
+
+`;
+
+console.log(letter);
